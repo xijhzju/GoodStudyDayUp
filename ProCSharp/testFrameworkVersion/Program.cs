be86c16bd1ae0f585283d0da33f8e8e10ce5f4bf
xijhzju@qq.com <xijhzju@qq.com>
2019-12-20 15:55:36 +0800

    modify testFrameworkVersion/Program.cs

--- ProCSharp/testFrameworkVersion/Program.cs
+++ ProCSharp/testFrameworkVersion/Program.cs
@@ -7,28 +7,27 @@ namespace testFrameworkVersion
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string[] myArgs = Environment.GetCommandLineArgs();//也可以得到命令行参数
-
-            Console.WriteLine($"myArgs.Length={myArgs.Length}");
-            Console.WriteLine($"args.Length={args.Length}");
-            // 从运行情况看：args就是实际的命令行参数构成的string数组
-            // 而myArgs的第一个成员myArgs[0]是对应程序dll的名字！之后才是具体的参数。
-            // 但看书中page121/1410的运行情况，myArgs中不包括dll的名字，也就是跟args一样。为啥？？
-            Console.WriteLine("myArgs:");
-            foreach (var str in myArgs)
-            {
-                Console.WriteLine(str);
-            }
-
-            Console.WriteLine("args：");
-            foreach (var str in args)
-            {
-                Console.WriteLine(str);
-            }
-
-
+            StringEquality();
+            Console.ReadKey();
+        }
+        static void StringEquality()
+        {
+            Console.WriteLine("=> String equality:");
+            string s1 = "Hello!";
+            string s2 = "Yo!";
+            Console.WriteLine("s1 = {0}", s1);
+            Console.WriteLine("s2 = {0}", s2);
+            Console.WriteLine();
+            // Test these strings for equality.
+            Console.WriteLine("s1 == s2: {0}", s1 == s2);
+            Console.WriteLine("s1 == Hello!: {0}", s1 == "Hello!");
+            Console.WriteLine("s1 == HELLO!: {0}", s1 == "HELLO!");
+            Console.WriteLine("s1 == hello!: {0}", s1 == "hello!");
+            Console.WriteLine("s1.Equals(s2): {0}", s1.Equals(s2));
+            Console.WriteLine("Yo.Equals(s2): {0}", "Yo!".Equals(s2));
+            Console.WriteLine();
         }
     }
 }
